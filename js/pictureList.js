4c9b1865a718508db11a64056e78790201b4adce
lgosoup <lgosup@naver.com>
2024-08-08 13:01:32 +0900

    my first commit

---
+++ js/pictureList.js
@@ -0,0 +1,39 @@
+const PICTURE_SRC_LIST = [
+  {
+    path: './image/photo1.jpg',
+    title: 'In front of my house',
+    subTitle: 'A cherry blossom festival is held every year',
+  },
+  {
+    path: './image/photo2.jpg',
+    title: 'My cousin',
+    subTitle: 'So cute',
+  },
+  {
+    path: './image/photo3.jpg',
+    title: 'Younger Sister',
+    subTitle: 'NO COMMENT.',
+  },
+  {
+    path: './image/photo4.jpg',
+    title: 'Cake sheet that I made',
+    subTitle: 'Use this to make a cake with whipped cream',
+  },
+  {
+    path: './image/photo5.jpg',
+    title: 'Namsan Seoul Tower',
+    subTitle: 'When I went on a trip',
+  },
+  {
+    path: './image/photo6.jpg',
+    title: 'The picture I drew',
+    subTitle: 'Three hours',
+  },
+  {
+    path: './image/photo7.jpeg',
+    title: 'The hackathon team',
+    subTitle: '',
+  },
+]
+
+export default PICTURE_SRC_LIST
